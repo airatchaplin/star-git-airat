8ec0c22617265a7e4bd2150c772ecdad7e51de1c
Airat_Salikhov <airatchaplin@gmail.com>
2020-09-02 14:01:09 +0300

    in stash

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,12 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello git!");
-        System.out.println("My name is Airat Salikhov");
-        System.out.println("How are you with git?");
-        System.out.println("This is good");
-        System.out.println("So, let's continue");
-        System.out.println("For revert");
-        System.out.println("Making progress");
-        System.out.println("Continue...");
+
     }
 }
